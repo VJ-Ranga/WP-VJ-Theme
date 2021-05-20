78b91359ac36902f760fa5f954f5e0fe7c2da6a9
VJRanga <viraj.madurange@gmail.com>
2021-05-21 03:40:28 +0530

    auto update

--- wp-vj-theme.php
+++ wp-vj-theme.php
@@ -3,7 +3,7 @@
  * Plugin Name: WP VJ Theme
  * Plugin URI: https://cloudycode.net/
  * Description: Wordpress Dashboard theme
- * Version: 0.1.0
+ * Version: 0.1.1
  * Author: VJRanga
  * Author URI: https://vjranga.com/
  * License: GPLv2 or later
