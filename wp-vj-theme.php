539a7816aea619b6cef5a1907ca155ca3e5fd2e2
VJRanga <viraj.madurange@gmail.com>
2022-01-08 14:49:15 +0530

    0.1.6
    0.1.6

--- wp-vj-theme.php
+++ wp-vj-theme.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://cloudycode.net/
  * Description: Simple WordPress Dashboard theme 
  * This plugin can change the Admin Dashboard, Admin Sidebar, classic post editor side panel, and login page of your WordPress website.
- * Version: 0.1.5
+ * Version: 0.1.6
  * Author: VJRanga
  * Author URI: https://vjranga.com/
  * License: GPLv2 or later
